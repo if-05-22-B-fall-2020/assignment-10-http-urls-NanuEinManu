98596984d4a44b1905aec59b5d1fd98716b08c69
NanuEinManu <cringynibba587@gmail.com>
2021-06-17 19:15:06 +0200

    Made it into a one-liner

--- src/main/java/HttpUrlChecker.java
+++ src/main/java/HttpUrlChecker.java
@@ -27,9 +27,6 @@ import java.util.regex.Pattern;
 
 public class HttpUrlChecker {
     public static Matcher getMatcher(String httpString) {
-        Pattern regex = Pattern.compile("^(http)s?://([a-z]|[0-9])*[.]([a-z]|[.]|[-])*$");
-
-        return regex.matcher(httpString);
+        return Pattern.compile("^(http)s?://([a-z]|[0-9])*[.]([a-z]|[.]|[-])*$").matcher(httpString);
     }
 }
-//2 3 6 7 10 | 4 8 9 11 12 | 1 13 | 5

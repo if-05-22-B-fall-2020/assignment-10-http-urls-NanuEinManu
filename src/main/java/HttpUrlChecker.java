7ab94a0773ee6bd53ff6c27ae2b987983d0d5da1
NanuEinManu <cringynibba587@gmail.com>
2021-06-17 19:11:56 +0200

    Tests 1, 13 laufen jetzt auch durch

--- src/main/java/HttpUrlChecker.java
+++ src/main/java/HttpUrlChecker.java
@@ -27,9 +27,9 @@ import java.util.regex.Pattern;
 
 public class HttpUrlChecker {
     public static Matcher getMatcher(String httpString) {
-        Pattern regex = Pattern.compile("^(http)s?://([a-z]|[0-9])*[.][a-z]*$");
+        Pattern regex = Pattern.compile("^(http)s?://([a-z]|[0-9])*[.]([a-z]|[.])*$");
 
         return regex.matcher(httpString);
     }
 }
-//2 3 6 7 10 | 4 8 9 11 12
+//2 3 6 7 10 | 4 8 9 11 12 | 1 13
